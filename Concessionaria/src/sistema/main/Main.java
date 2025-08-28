a01a66cbbf0a5bb621ef769d05a2f92a8b85bfaf
manelbsz <emanuel.bzrr.santos@gmail.com>
2025-08-28 15:13:32 -0300

    style: ajuste do caminho para leitura do arquivo

--- Concessionaria/src/sistema/main/Main.java
+++ Concessionaria/src/sistema/main/Main.java
@@ -12,8 +12,8 @@ public class Main {
         FachadaGerente gerente = new FachadaGerente("João Gerente", "11122233344", LocalDate.of(1974, 3, 5));
         FachadaVendedor vendedor = new FachadaVendedor("Maria Vendedora", "55566677788", LocalDate.of(1989, 3, 20)); // retirar isso urgente
         
-        minhaConcessionaria.carregarVeiculos("Concessionaria\\src\\concessionaria\\repositorios\\veiculos.txt");
-        minhaConcessionaria.carregarClientes("Concessionaria\\src\\concessionaria\\repositorios\\clientes.txt");
+        minhaConcessionaria.carregarVeiculos("Concessionaria\\src\\sistema\\repositorios\\veiculos.txt");
+        minhaConcessionaria.carregarClientes("Concessionaria\\src\\sistema\\repositorios\\clientes.txt");
 
         // Instanciar e iniciar a nova classe de menu
         MenuPrincipal menu = new MenuPrincipal(minhaConcessionaria, gerente, vendedor);
